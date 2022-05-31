d440422ca5b24487c1ced3d75b6e2d2906f26ebd
Mauricio Rojas <a20171181@pucp.edu.pe>
2022-05-30 23:11:26 -0500

    pedido masivo

--- src/views/apps/block/add/index.js
+++ src/views/apps/block/add/index.js
@@ -13,6 +13,7 @@ import '@styles/react/libs/tables/react-dataTable-component.scss'
 const Tables = () => {
 
     const [bloqueos, setBloqueos] = useState([])
+    const [pedidos, setPedidos] = useState([])
 
     const showFile = (e) => {
         e.preventDefault()
@@ -48,8 +49,41 @@ const Tables = () => {
                 bloqueo.fecha_fin = d2
                 bloques = [...bloques, bloqueo]
             }
-            setBloqueos(bloques)
+
             console.log(bloques)
+            setBloqueos(bloques)
+
+        }
+        reader.readAsText(e.target.files[0])
+    }
+
+    const pedidoMasivo = (e) => {
+        e.preventDefault()
+        const reader = new FileReader()
+        reader.onload = (e) => {
+            let pedidos = []
+            const text = e.target.result
+            const line = text.split('\n')
+            //01 00:56, 150101 =>  020501,  77, 000484
+            for (let i = 0; i < line.length; i++) {
+                const d = new Date()
+                
+                const part = line[i].split(/(\s+)/).filter(e => e.length > 1)
+                if (part.length < 1) break
+                const pedido = { id: 0, idCliente: 0, cantPaquetes: d, cantPaquetesNoAsignado: 0, idCiudadDestino: 0, fechaHoraCreacion: d, estado: 0 }
+                pedido.idCiudadDestino = parseInt(part[5].slice(0, -1))
+                pedido.cantPaquetes = parseInt(part[6].slice(0, -1))
+                pedido.idCliente = parseInt(part[7].slice(0, -1))
+                d.setDate(parseInt(part[0]))
+                const hora = part[1].split(":")
+                d.setHours(parseInt(hora[0]), parseInt(hora[1].slice(0, -1)))
+                pedido.fechaHoraCreacion = d
+                pedidos = [...pedidos, pedido]
+            }
+
+            console.log(pedidos)
+            setPedidos(pedidos)
+
         }
         reader.readAsText(e.target.files[0])
     }
@@ -61,6 +95,12 @@ const Tables = () => {
                 <FormGroup>
                 <Label for='inputFile'>carga de bloqueos masivo</Label>
                 <Input type='file' id='inputFile' name='fileInpur' onChange={showFile}/>
+                </FormGroup>
+            </Col>
+            <Col sm='12'>
+                <FormGroup>
+                <Label for='inputFile'>carga de pedidos masivo</Label>
+                <Input type='file' id='inputFile' name='fileInpur' onChange={pedidoMasivo}/>
                 </FormGroup>
             </Col>
         </Row>
